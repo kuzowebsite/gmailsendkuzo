0c7331a496d0a3e965585afb3e23559a1650c954
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2024-09-11 03:52:44 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function SendMail() {
+    var params = {
+        from_name : document.getElementById("name").value,
+        email_id : document.getElementById("email_id").value,
+        torol : document.getElementById("torol").value,
+        class : document.getElementById("class").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_tto1xyq", "template_6z9nvqr", params).then(function (res) {
+        alert("Амжилтай илгээлээ ! " + res.status);
+    })
+}
