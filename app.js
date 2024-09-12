300015568e904bd79e3ef92a2768ba040cf44cae
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2024-09-12 08:44:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ function SendMail() {
     var params = {
         from_name : document.getElementById("name").value,
         email_id : document.getElementById("email_id").value,
+        dugaar_id : document.getElementById("dugaar_id").value,
         torol : document.getElementById("torol").value,
         class : document.getElementById("class").value,
-        message : document.getElementById("message").value
+        message : document.getElementById("message").value,
     }
-    emailjs.send("service_tto1xyq", "template_6z9nvqr", params).then(function (res) {
+    emailjs.send("service_7pihjz4", "template_j8qz4jg", params).then(function (res) {
         alert("Амжилтай илгээлээ ! " + res.status);
     })
 }
