8c0bc811cdbb85baf41c62d093db67f260ec03b2
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2024-09-12 20:18:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Email sending function
 function SendMail() {
     var params = {
         from_name : document.getElementById("name").value,
@@ -6,35 +7,38 @@ function SendMail() {
         torol : document.getElementById("torol").value,
         class : document.getElementById("class").value,
         time : document.getElementById("time").value,
-	dugnelt : document.getElementById("dugnelt").value,
+        dugnelt : document.getElementById("dugnelt").value,
         message : document.getElementById("message").value,
     }
-    emailjs.send("service_7pihjz4", "template_j8qz4jg", params).then(function (res) {
-        alert("Амжилтай илгээлээ ! " + res.status);
 
-        const btn = document.querySelector(".button");
-        btn.classList.add("button--loading");
-        btn.classList.remove("button--loading");
-    })
-} 
+    // Validate form before sending email
+    if (validateForm()) {
+        emailjs.send("service_7pihjz4", "template_j8qz4jg", params).then(function (res) {
+            alert("Амжилтай илгээлээ ! " + res.status);
+            const btn = document.querySelector(".button");
+            btn.classList.add("button--loading");
+            setTimeout(() => btn.classList.remove("button--loading"), 1000);
+        });
+    }
+}
 
+// Menu toggle functions
 var theToggle = document.getElementById('toggle');
 
-
 function hasClass(elem, className) {
-	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
 }
 
 function addClass(elem, className) {
     if (!hasClass(elem, className)) {
-    	elem.className += ' ' + className;
+        elem.className += ' ' + className;
     }
 }
 
 function removeClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
-	if (hasClass(elem, className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0) {
             newClass = newClass.replace(' ' + className + ' ', ' ');
         }
         elem.className = newClass.replace(/^\s+|\s+$/g, '');
@@ -42,10 +46,10 @@ function removeClass(elem, className) {
 }
 
 function toggleClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
     if (hasClass(elem, className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0 ) {
-            newClass = newClass.replace( " " + className + " " , " " );
+        while (newClass.indexOf(" " + className + " ") >= 0) {
+            newClass = newClass.replace(" " + className + " ", " ");
         }
         elem.className = newClass.replace(/^\s+|\s+$/g, '');
     } else {
@@ -54,32 +58,42 @@ function toggleClass(elem, className) {
 }
 
 theToggle.onclick = function() {
-   toggleClass(this, 'on');
-   return false;
+    toggleClass(this, 'on');
+    return false;
 }
 
-//aldaa border
+// Хоосон талбар дээрх улаан хүрээг баталгаажуулах функц
 function validateInput(input) {
     if (input.value.trim() === "") {
-      input.classList.add("error");
+        input.classList.add("error");
     } else {
-      input.classList.remove("error");
+        input.classList.remove("error");
     }
-  }
-  
-  const inputs = document.querySelectorAll(".input-field");
-  
-  inputs.forEach(input => {
-    input.addEventListener("blur", function() {
-      validateInput(input);
+}
+
+// Маягтыг бүхэлд нь баталгаажуулна
+function validateForm() {
+    const inputs = document.querySelectorAll(".input-field");
+    let isValid = true;
+
+    inputs.forEach(input => {
+        validateInput(input);
+        if (input.classList.contains("error")) {
+            isValid = false;
+        }
     });
-  });
-  
-  document.getElementById("container").addEventListener("send", function(e) {
-    inputs.forEach(input => validateInput(input));
-    
-    if (document.querySelectorAll(".error").length > 0) {
-      e.preventDefault();
-      alert("Бүх талбарыг бөглөнө үү.");
+
+    if (!isValid) {
+        alert("Бүх талбарыг бөглөнө үү.");
     }
-  });
+
+    return isValid;
+}
+
+// анх орход бас өглөж байх үед бүгдийн бөглсөн үед blur асан
+const inputs = document.querySelectorAll(".input-field");
+inputs.forEach(input => {
+    input.addEventListener("blur", function() {
+        validateInput(input);
+    });
+});
