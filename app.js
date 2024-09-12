9daa4ce92d3f19a67fc52c40e4f137a3a25db69d
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2024-09-12 12:07:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,5 +9,49 @@ function SendMail() {
     }
     emailjs.send("service_7pihjz4", "template_j8qz4jg", params).then(function (res) {
         alert("Амжилтай илгээлээ ! " + res.status);
+
+        const btn = document.querySelector(".button");
+        btn.classList.add("button--loading");
+        btn.classList.remove("button--loading");
     })
+} 
+
+var theToggle = document.getElementById('toggle');
+
+
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
 }
